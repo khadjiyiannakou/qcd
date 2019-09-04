3401aa9892c709297f731844a5216efb56285389
Kyriakos Hadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2019-09-04 05:41:50 -0400

    Add Vqg needs check again

---
+++ zfac_Vqg.c
@@ -0,0 +1,170 @@
+// Kyriakos Hadjiyiannakou 
+ 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <mpi.h>
+#include <qcd.h>
+
+
+int main(int argc,char* argv[])
+{
+
+   qcd_uint_2 mu,nu,rho,ku,lu,c1,c2;          // various loop variables
+   qcd_uint_4 i,j,k,lt,x,y,z,t,ip1,im1; 
+   qcd_uint_2 id1,id2,id3,id4;
+   qcd_uint_2 ic1,ic2,ic3,ic4;                    
+   qcd_real_8 tmp;                            // general purpuse
+   qcd_uint_2 xx[4];
+
+   FILE *fp_gprops; // read gluon propagator
+
+   FILE *fp_vfun_vD;
+   FILE *fp_loops_vD;
+
+   int params_len;               // needed to read inputfiles
+   char *params;                 // needed to read inputfiles
+
+
+   char vfun_name_vD[qcd_MAX_STRING_LENGTH];
+
+   char param_name[qcd_MAX_STRING_LENGTH];      // name of parameter file
+
+   qcd_geometry geo;                            // geometry structure
+   char gprop_name[qcd_MAX_STRING_LENGTH];       // name of inverted Fourier source
+   char loops_name_vD[qcd_MAX_STRING_LENGTH];
+
+   qcd_real_8 theta[4] = {M_PI,0.0,0.0,0.0};    // antiperiodic b.c. in time
+   qcd_uint_2 L[4];
+   qcd_uint_2 P[4];
+   
+   qcd_complex_16 gprop[4][4][3][3];
+
+   qcd_real_8 loops_vD[10];
+
+
+   int myid,numprocs, namelen;    
+   char processor_name[MPI_MAX_PROCESSOR_NAME];     
+   				 
+   int d_dummy;
+   //////////////////////////////////////////////////////////////////////////////////////          
+   //set up MPI
+   MPI_Init(&argc, &argv);
+   MPI_Comm_size(MPI_COMM_WORLD,&numprocs);         // num. of processes taking part in the calculation
+   MPI_Comm_rank(MPI_COMM_WORLD,&myid);             // each process gets its ID
+   MPI_Get_processor_name(processor_name,&namelen); //    
+   
+   
+   //////////////////// READ INPUT FILE /////////////////////////////////////////////
+      
+   if(argc!=2)
+   {
+      if(myid==0) fprintf(stderr,"No input file specified\n");
+      exit(EXIT_FAILURE);
+   }
+
+   strcpy(param_name,argv[1]);
+   if(myid==0)
+   {
+      i=0;
+      printf("opening input file %s\n",param_name);
+      params=qcd_getParams(param_name,&params_len);
+      if(params==NULL)
+      {
+         i=1;
+      }
+   }
+   MPI_Bcast(&i,1,MPI_INT, 0, MPI_COMM_WORLD);
+   if(i==1) exit(EXIT_FAILURE);
+   MPI_Bcast(&params_len, 1, MPI_INT, 0, MPI_COMM_WORLD);
+   if(myid!=0) params = (char*) malloc(params_len*sizeof(char));
+   MPI_Bcast(params, params_len, MPI_CHAR, 0, MPI_COMM_WORLD);
+   
+   sscanf(qcd_getParam("<processors_txyz>",params,params_len),"%hd %hd %hd %hd",&P[0], &P[1], &P[2], &P[3]);
+   sscanf(qcd_getParam("<lattice_txyz>",params,params_len),"%hd %hd %hd %hd",&L[0], &L[1], &L[2], &L[3]);
+   if(qcd_initGeometry(&geo,L,P, theta, myid, numprocs)) exit(EXIT_FAILURE);
+   
+   if(myid==0) printf(" Local lattice: %i x %i x %i x %i\n",geo.lL[0],geo.lL[1],geo.lL[2],geo.lL[3]);
+
+   strcpy(loops_name_vD,qcd_getParam("<loops_name_vD>",params,params_len));
+   if(myid==0) printf("Got loop file name: %s\n",loops_name_vD);
+
+   strcpy(vfun_name_vD,qcd_getParam("<vfd_vD_name>",params,params_len));
+   if(myid==0) printf("Got output file name: %s\n",vfun_name_vD);
+
+   strcpy(gprop_name,qcd_getParam("<gprop_name>",params,params_len));
+   if(myid==0) printf("Got output file name: %s\n",gprop_name);
+            
+    
+   free(params);
+   
+
+   if(P[0]*P[1]*P[2]*P[3] != 1){
+     fprintf(stderr,"Error only one process is allowed\n");
+     exit(EXIT_FAILURE);
+   }
+   //#####################################################################   
+
+   if(myid==0)
+   {
+      j=0;
+      if( (fp_vfun_vD=fopen(vfun_name_vD,"w"))==NULL) j++;
+      if( (fp_loops_vD=fopen(loops_name_vD,"r"))==NULL) j++;
+      if( (fp_gprops=fopen(gprop_name,"r"))==NULL ) j++;
+      if(j>0) fprintf(stderr,"Error while opening input/output files!\n");
+   }
+   MPI_Bcast(&j,1,MPI_INT, 0, MPI_COMM_WORLD);
+   if(j>0) exit(EXIT_FAILURE);
+   
+      
+   for(mu = 0 ; mu < 4 ; mu++)
+     for(nu = 0 ; nu < 4 ; nu++)
+       for(c1 = 0 ; c1 < 3 ; c1++)
+	 for(c2 = 0 ; c2 < 3 ; c2++){
+	   fscanf(fp_gprops,"%d %d %d %d %lf %lf",&d_dummy,&d_dummy,&d_dummy,&d_dummy,&(gprop[mu][nu][c1][c2].re), &(gprop[mu][nu][c1][c2].im));
+	 }
+
+   ////////////////////////////////////////////////////////////////////////////////////////////////////////
+     
+     for(int i = 0 ; i < 10 ; i++) fscanf(fp_loops_vD,"%lf",&(loops_vD[i]));
+
+     for(int i = 0 ; i < 10 ; i++)
+       for(mu=0; mu<4; mu++)
+	 for(nu=0; nu<4; nu++)
+	   for(c1=0; c1<3; c1++)
+	     for(c2=0; c2<3; c2++)
+	       {
+		 fprintf(fp_vfun_vD,"%d \t %d %d %d %d \t %+e %+e\n",i,mu,nu,c1,c2,gprop[mu][nu][c1][c2].re * loops_vD[i], gprop[mu][nu][c1][c2].im * loops_vD[i]);
+	       }
+
+   
+   qcd_destroyGeometry(&geo);
+   MPI_Finalize();
+}//end main 
+
+   /*
+   FILE *fp_uprop;
+   FILE *fp_dprop;
+   
+   fp_uprop = fopen("uprop.0100.dat","w");
+   fp_dprop = fopen("dprop.0100.dat","w");
+
+   for(t=0; t<geo.lL[0]; t++)
+     for(x=0; x<geo.lL[1]; x++)
+       for(y=0; y<geo.lL[2]; y++)
+	 for(z=0; z<geo.lL[3]; z++)
+	   {
+	     int iv = qcd_LEXIC(t,x,y,z,geo.lL);
+	     for(mu=0; mu<4; mu++)
+	       for(nu=0; nu<4; nu++)
+		 for(c1=0; c1<3; c1++)
+		   for(c2=0; c2<3; c2++)
+		     {
+		       fprintf(fp_uprop,"%+16.15e %+16.15e\n",uprop.D[iv][mu][nu][c1][c2].re, uprop.D[iv][mu][nu][c1][c2].im);
+		       fprintf(fp_dprop,"%+16.15e %+16.15e\n",dprop.D[iv][mu][nu][c1][c2].re, dprop.D[iv][mu][nu][c1][c2].im);
+		     }
+	   }
+   exit(-1);
+   */
+   ////////////////////
