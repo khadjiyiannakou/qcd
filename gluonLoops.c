377f249cbbecc96527c4040e80dcada4fefe5f81
Kyriakos Hadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2019-09-04 05:43:48 -0400

    Added option for trace anomaly and option for traceless gluon fields

--- gluonLoops.c
+++ gluonLoops.c
@@ -19,6 +19,8 @@ int main(int argc,char* argv[])
    char   output_name[qcd_MAX_STRING_LENGTH];
    char momlist_name[qcd_MAX_STRING_LENGTH];
    char sumT[qcd_MAX_STRING_LENGTH]; // if this string is yes then it sums the time slices
+   char doSg[qcd_MAX_STRING_LENGTH]; // if this string is yes we will compute also Sg for trace anomaly
+   char doTraceless[qcd_MAX_STRING_LENGTH]; // In case we want to make the traceless the field after 
 
    qcd_int_4   i,k,j,nsmearstout;
    qcd_int_4 printStep;
@@ -105,6 +107,15 @@ int main(int argc,char* argv[])
    strcpy(sumT,qcd_getParam("<sumT>",params,params_len));
    if(myid==0) printf("Do you want to perform the sum over time-slices?: %s\n",sumT);
 
+   strcpy(doSg,qcd_getParam("<doSg>",params,params_len));
+   if(myid==0) printf("Do you want to compute also trace anomaly?: %s\n",doSg);
+
+   strcpy(doTraceless,qcd_getParam("<doTraceless>",params,params_len));
+   if(myid==0) printf("Do you want to use traceless in the gluon fields?: %s\n",doTraceless);
+
+
+
+
    if(myid==0)
    {
       pfile = fopen(output_name,"w");   
@@ -155,6 +166,7 @@ int main(int argc,char* argv[])
    qcd_initGaugeField(&u_out,&geo);
    qcd_initGaugeField(&utmp,&geo);
 
+
    if(qcd_getGaugeField(gauge_name,qcd_GF_LIME,&u))
      {
        fprintf(stderr,"process %i: Error reading gauge field!\n",myid);
@@ -163,13 +175,28 @@ int main(int argc,char* argv[])
        
    if(myid==0) printf("gauge-field loaded\n");
 
+   if( strcmp(doTraceless,"yes") == 0  || strcmp(doTraceless,"YES") == 0){
+     traceless_gluon_Field(&utmp,&u);
+     qcd_copyGaugeField(&u,&utmp);
+   }
+
    qcd_communicateGaugePM(&u);
    qcd_waitall(&geo);
    plaq = qcd_calculatePlaquette(&u);
    if(myid==0) printf("plaquette not smeared= %e\n",plaq);
-
+     
    qcd_complex_16 *gLoops = malloc(Nprint*geo.lL[0]*Nmom*2*sizeof(double));
-   qcd_calculateGluonLoops(&u,gLoops,mom,Nmom); // gluon loops without any smearing
+   qcd_complex_16 *gLoops_Sg;
+   if( strcmp(doSg,"yes") == 0  || strcmp(doSg,"YES") == 0)
+     gLoops_Sg = malloc(Nprint*geo.lL[0]*Nmom*2*sizeof(double));
+   else
+     gLoops_Sg = NULL;
+
+   if( strcmp(doSg,"yes") == 0  || strcmp(doSg,"YES") == 0)
+     qcd_calculateGluonLoops_T00g_Sg(&u,gLoops,gLoops_Sg,mom,Nmom);
+   else
+     qcd_calculateGluonLoops(&u,gLoops,mom,Nmom); // gluon loops without any smearing
+
    qcd_copyGaugeField(&utmp,&u);
 
    int ii=0;
@@ -181,11 +208,15 @@ int main(int argc,char* argv[])
        qcd_copyGaugeField(&utmp,&u_out);
        if((i+1)%printStep == 0){
 	 ii+=1;
-	 qcd_calculateGluonLoops(&u_out,&(gLoops[ii*geo.lL[0]*Nmom]),mom,Nmom);
+	 if( strcmp(doSg,"yes") == 0  || strcmp(doSg,"YES") == 0)
+	   qcd_calculateGluonLoops_T00g_Sg(&u_out,&(gLoops[ii*geo.lL[0]*Nmom]),&(gLoops_Sg[ii*geo.lL[0]*Nmom]),mom,Nmom);
+	 else
+	   qcd_calculateGluonLoops(&u_out,&(gLoops[ii*geo.lL[0]*Nmom]),mom,Nmom);
        }
      }   
 
    qcd_complex_16  sumLoop;
+   qcd_complex_16 sumLoopSg;
 
    if( strcmp(sumT,"yes") == 0  || strcmp(sumT,"YES") == 0){
      if(myid == 0) printf("You have enable sum of the loops on time");
@@ -193,11 +224,23 @@ int main(int argc,char* argv[])
        for(int imom = 0 ; imom < Nmom ; imom++){
 	 sumLoop.re=0.; sumLoop.im=0.;
 	 if(myid==0){
-	   for(int it =0 ; it < geo.lL[0] ; it++){
-	     sumLoop.re += gLoops[st*geo.lL[0]*Nmom + it*Nmom + imom].re;
-	     sumLoop.im += gLoops[st*geo.lL[0]*Nmom + it*Nmom + imom].im;
-	   } 
-	   fprintf(pfile,"%d %+d %+d %+d %+e %+e\n",st*printStep,mom[imom][0],mom[imom][1],mom[imom][2],sumLoop.re,sumLoop.im);
+	   if( strcmp(doSg,"yes") == 0  || strcmp(doSg,"YES") == 0){
+	     for(int it =0 ; it < geo.lL[0] ; it++){
+	       sumLoop.re += gLoops[st*geo.lL[0]*Nmom + it*Nmom + imom].re;
+	       sumLoop.im += gLoops[st*geo.lL[0]*Nmom + it*Nmom + imom].im;
+	       sumLoopSg.re += gLoops_Sg[st*geo.lL[0]*Nmom + it*Nmom + imom].re;
+	       sumLoopSg.im += gLoops_Sg[st*geo.lL[0]*Nmom + it*Nmom + imom].im;
+	     } 
+	     fprintf(pfile,"%d %+d %+d %+d %+e %+e %+e %+e\n",st*printStep,mom[imom][0],mom[imom][1],mom[imom][2],sumLoop.re,sumLoop.im,
+		     sumLoopSg.re,sumLoopSg.im);
+	   }
+	   else{
+	     for(int it =0 ; it < geo.lL[0] ; it++){
+	       sumLoop.re += gLoops[st*geo.lL[0]*Nmom + it*Nmom + imom].re;
+	       sumLoop.im += gLoops[st*geo.lL[0]*Nmom + it*Nmom + imom].im;
+	     } 
+	     fprintf(pfile,"%d %+d %+d %+d %+e %+e\n",st*printStep,mom[imom][0],mom[imom][1],mom[imom][2],sumLoop.re,sumLoop.im);
+	   }
 	 }
        }
    }
@@ -205,12 +248,19 @@ int main(int argc,char* argv[])
      for(int st = 0 ; st < Nprint ; st++)
        for(int it =0 ; it < geo.lL[0] ; it++)
 	 for(int imom = 0 ; imom < Nmom ; imom++)
-	   if(myid==0)
-	     fprintf(pfile,"%d %d %+d %+d %+d %+e %+e\n",st*printStep,it,mom[imom][0],mom[imom][1],mom[imom][2],
-		     gLoops[st*geo.lL[0]*Nmom + it*Nmom + imom].re, gLoops[st*geo.lL[0]*Nmom + it*Nmom + imom].im);
+	   if(myid==0){
+	     if( strcmp(doSg,"yes") == 0  || strcmp(doSg,"YES") == 0)
+	       fprintf(pfile,"%d %d %+d %+d %+d %+e %+e %+e %+e\n",st*printStep,it,mom[imom][0],mom[imom][1],mom[imom][2],
+		       gLoops[st*geo.lL[0]*Nmom + it*Nmom + imom].re, gLoops[st*geo.lL[0]*Nmom + it*Nmom + imom].im,
+		       gLoops_Sg[st*geo.lL[0]*Nmom + it*Nmom + imom].re, gLoops_Sg[st*geo.lL[0]*Nmom + it*Nmom + imom].im);	       
+	     else
+	       fprintf(pfile,"%d %d %+d %+d %+d %+e %+e\n",st*printStep,it,mom[imom][0],mom[imom][1],mom[imom][2],
+		       gLoops[st*geo.lL[0]*Nmom + it*Nmom + imom].re, gLoops[st*geo.lL[0]*Nmom + it*Nmom + imom].im);
+	   }
    }
 
    free(gLoops);
+   free(gLoops_Sg);
    free(mom);
    qcd_destroyGaugeField(&utmp); 
    qcd_destroyGaugeField(&u_out); 
